54cd46259d5eeeacabc9b7cbecdd2bda4d83e81d
MyPoZi <kxx555u.u@gmail.com>
2018-08-01 23:48:29 +0900

    swiper追加

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(function () {
+  var nav = $('.navbar');
+  var navTop = nav.offset().top;
+  $(window).scroll(function () {
+    var winTop = $(this).scrollTop();
+    if (winTop >= navTop) {
+      nav.addClass('fixed')
+    } else if (winTop <= navTop) {
+      nav.removeClass('fixed')
+    }
+  });
+});
